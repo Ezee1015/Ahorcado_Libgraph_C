522db9eb27aa15891b2449023f57706ad6175e92
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2021-10-12 20:08:35 +0000

    Update Ahorcado.c

--- Ahorcado.c
+++ Ahorcado.c
@@ -84,58 +84,56 @@ void Pregunta (char *Palabra, int *Cantidad_de_Letras) {
 
 
 void Lee_Letra (char *Letra_Ingresada) {
-    
-      printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                               ");   
-     outtextxy(20,395,"Ingrese la Letra a Adivinar=>");
+    outtextxy(20,395,"Ingrese la Letra a Adivinar=>");
     fflush(stdin);
     scanf("%c", Letra_Ingresada);
     fflush(stdin);
-//     printf("%c", *Letra_Ingresada);  
     fflush(stdin);    
     
 }
 
 
-void Comprobar_y_Colocar_Letra (char Letra_Ingresada, char *Palabra, int Cantidad_de_Letras, int Largo_Cada_Linea, int Espacio_por_cada_Linea, int *Total_Encontradas, int PosX_Ahorcado, int PosY_Ahorcado) {
-    
-    int Cant_Letras_Encontradas=0, i;
+void Comprobar_y_Colocar_Letra (char Letra_Ingresada, char *Palabra, int Cantidad_de_Letras, int Largo_Cada_Linea, int Espacio_por_cada_Linea, int *Total_Encontradas, int *Fallos, int PosX_Ahorcado, int PosY_Ahorcado) {
+
+    int Cant_Letras_Encontradas=0, i, r, z;
     
     for(i=0;i<=Cantidad_de_Letras;i++) {
     
-        if(Palabra[i]==Letra_Ingresada) {
-            Cant_Letras_Encontradas++;
-            Imprimir_Letra(Palabra, i, Letra_Ingresada, Largo_Cada_Linea, Espacio_por_cada_Linea, PosX_Ahorcado, PosY_Ahorcado); //COMPLETAR ESTE LLAMADO DE FUNCION
-        }
+            if(Palabra[i]==Letra_Ingresada) {
+                Cant_Letras_Encontradas++;
+                
+                Imprimir_Letras (Palabra, i, Letra_Ingresada, Largo_Cada_Linea, Espacio_por_cada_Linea, PosX_Ahorcado, PosY_Ahorcado); //COMPLETAR ESTE LLAMADO DE FUNCION
+            }
+        
         
     }
+        if (Cant_Letras_Encontradas==0) (*Fallos)++;
+        
         *Total_Encontradas=(*Total_Encontradas)+Cant_Letras_Encontradas;
         //printf("%i",Cant_Letras_Encontradas);
         
 }
 
 
-void Imprimir_Letra (char *Palabra, int Posicion, char Letra_Ingresada, int Largo_Cada_Linea, int Espacio_por_cada_Linea, int PosX_Ahorcado, int PosY_Ahorcado) {
+void Imprimir_Letras (char *Palabra, int Posicion, char Letra_Ingresada, int Largo_Cada_Linea, int Espacio_por_cada_Linea, int PosX_Ahorcado, int PosY_Ahorcado) {
     //ACÁ TENDRÍA QUE IMPRIMIR LA LETRA QUE SE ENCONTRO SEGUN LA POSICION EN EL STRING DE 'PALABRA'. LA POSICION EN EL MAPA SE PUEDE SABER CON EL VALOR DE 'LARGO_DE_LINEA' QUE SE DIVIDE A LA MITAD Y LOS ESPACIO_POR_cada_Linea PARA CALCULAR EL ESPACIO JUSTO DONDE LLEGA (PORQUE SINO NO VA A CONTAR LOS ESPACIO Y VA A TERMINAL MAL)
-    
     char Letra[120];
-    sprintf(Letra,"%c",Letra_Ingresada); //COLOCA EN LA VARIABLE 'Letra' EL FORMATO CORRECTO PARA QUE OUTTEXTXY PUEDA IMPRIMIR CORRECTAMENTE 'Letra_Ingresada'
-    
-//     printf("%i", PosX_Ahorcado + (Largo_Cada_Linea/2) + ((Largo_Cada_Linea + Espacio_por_cada_Linea)*Posicion) );
-    
-    
-    outtextxy( (PosX_Ahorcado + Largo_Cada_Linea/2 + Largo_Cada_Linea*Posicion + Espacio_por_cada_Linea*Posicion) - 1 , PosY_Ahorcado-30 ,   Letra   );
-    
-    
-    
-    
+
+            sprintf(Letra,"%c",Palabra[Posicion]); //COLOCA EN LA VARIABLE 'Letra' EL FORMATO CORRECTO PARA QUE OUTTEXTXY PUEDA IMPRIMIR CORRECTAMENTE 'Letra_Ingresada'
+            
+        //     printf("%i", PosX_Ahorcado + (Largo_Cada_Linea/2) + ((Largo_Cada_Linea + Espacio_por_cada_Linea)*Posicion) );
+            
+            
+            outtextxy( (PosX_Ahorcado + Largo_Cada_Linea/2 + Largo_Cada_Linea*Posicion + Espacio_por_cada_Linea*Posicion) - 1 , PosY_Ahorcado-30 ,   Letra   );
+
 }
 
 
 
     
 int main () {
-    int gdriver=DETECT, gmode, i, Cantidad_de_Letras=0,Largo_Cada_Linea,Espacio_por_cada_Linea, Total_Encontradas=0;
-    char Palabra[99], Letra_Ingresada='~';
+    int gdriver=DETECT, gmode, i, Cantidad_de_Letras=0,Largo_Cada_Linea,Espacio_por_cada_Linea, Total_Encontradas=0, Fallos=0;
+    char Palabra[99999], Respuesta[99999], Letra_Ingresada='~';
     initgraph(&gdriver,&gmode,NULL);
     
     
@@ -143,22 +141,38 @@ int main () {
         Palabra[i]='~';
     }
     
+    
     //Menu_Inicial();
     
      Pregunta(&Palabra,&Cantidad_de_Letras);
-    
+    /*
     closegraph(); // CIERRO Y VUELVO A ABRIR PARA QUE NO SE CUELGUE LA LECTURA DEL TEXTO
     gdriver=DETECT; gmode=7589738954937;
     initgraph(&gdriver,&gmode,NULL);
+    */
     
-    //while(Juego_Terminado!=1){
-    Poste_Ahorcado(60,20,120);
-    Lineas_Palabras(100,350,getmaxx()-100,Cantidad_de_Letras, &Largo_Cada_Linea,&Espacio_por_cada_Linea);
-    Lee_Letra(&Letra_Ingresada);
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                               ");   
+      
+    while(Fallos<5){
+        Poste_Ahorcado(Fallos,20,120);
+        Lineas_Palabras(100,350,getmaxx()-100,Cantidad_de_Letras, &Largo_Cada_Linea,&Espacio_por_cada_Linea);
+        Lee_Letra(&Letra_Ingresada);
+        Comprobar_y_Colocar_Letra(Letra_Ingresada,&Palabra,Cantidad_de_Letras,Largo_Cada_Linea,Espacio_por_cada_Linea, &Total_Encontradas, &Fallos, 100, 350);
+        if (Total_Encontradas==Cantidad_de_Letras) {
+            cleardevice();
+            outtextxy(195,65,"FELICIDADES --> Has Ganado");
+            delay(3000);
+            closegraph();
+        }
+    }
     
+    cleardevice();
+    outtextxy(195,65,"Lastima, Has PERDIDO");
+    outtextxy(150,85,"Talvez tengas mayor suerte la proxima.");
+    outtextxy(195,115,"RESPUESTA --> ");
+    sprintf(Respuesta,"%s",Palabra);
+    outtextxy(305,115,Respuesta);
     
-    Comprobar_y_Colocar_Letra(Letra_Ingresada,&Palabra,Cantidad_de_Letras,Largo_Cada_Linea,Espacio_por_cada_Linea, &Total_Encontradas, 100/*POSICION 'x' DE LAS LINEAS DEL AHORCADO*/, 350/*POSICION 'y' DE LAS LINEAS DEL AHORCADO*/);
-
     delay(3000);
     closegraph();
     
