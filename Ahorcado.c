d29e15d92eb6a0bf48d2cf1959dcf50eb29c8899
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2021-10-12 18:27:47 +0000

    Update Ahorcado.c

--- Ahorcado.c
+++ Ahorcado.c
@@ -31,21 +31,20 @@ void Poste_Ahorcado (int Fallidos, int PosX, int PosY) {
 }
 
 
-void Lineas_Palabras(PosX, PosY, TotalX, Cantidad) {
- 
-    int Largo_Cada_Linea=(TotalX/Cantidad), 
-        i,
-        Espacio_por_cada_Linea=Largo_Cada_Linea*20/100;
-    Largo_Cada_Linea=Largo_Cada_Linea-Espacio_por_cada_Linea;
+void Lineas_Palabras(int PosX, int PosY, int TotalX, int Cantidad, int *Largo_Cada_Linea, int *Espacio_por_cada_Linea) {
+    int  i;
+    *Largo_Cada_Linea=(TotalX/Cantidad); 
+    *Espacio_por_cada_Linea=(*Largo_Cada_Linea)*20/100;
+    *Largo_Cada_Linea=(*Largo_Cada_Linea)-(*Espacio_por_cada_Linea);
  
 //     line(PosX,PosY,TotalX,PosY);
     
 //     printf("\n\n        %i  -  %i", PosX, PosX+Largo_Cada_Linea);
-    line(PosX,PosY,PosX+Largo_Cada_Linea,PosY);
+    line(PosX,PosY,PosX+(*Largo_Cada_Linea),PosY);
     
     for(i=1;i<Cantidad;i++) {
 //     printf("\n\n        %i  -  %i", PosX+Largo_Cada_Linea*i+Espacio_por_cada_Linea*i, PosX+Largo_Cada_Linea*(i+1)+Espacio_por_cada_Linea*i);
-        line(PosX+Largo_Cada_Linea*i+Espacio_por_cada_Linea*i,PosY,PosX+Largo_Cada_Linea*(i+1)+Espacio_por_cada_Linea*i,PosY);
+        line(PosX+(*Largo_Cada_Linea)*i+(*Espacio_por_cada_Linea)*i,PosY,PosX+(*Largo_Cada_Linea)*(i+1)+(*Espacio_por_cada_Linea)*i,PosY);
     }
 
     
@@ -68,7 +67,9 @@ void Pregunta (char *Palabra, int *Cantidad_de_Letras) {
     
     outtextxy(20,215,"Ingrese la Palabra del Ahorcado=> ");
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                                       ");
+    fflush(stdin);
     scanf("%s",Palabra);
+    fflush(stdin);
     cleardevice();
     
     for(i=0;i<99;i++) {
@@ -80,27 +81,85 @@ void Pregunta (char *Palabra, int *Cantidad_de_Letras) {
 //     printf("\n\n\n\n            Cantidad de letras: %i", *Cantidad_de_Letras);
     
 }
+
+
+void Lee_Letra (char *Letra_Ingresada) {
+    
+      printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                               ");   
+     outtextxy(20,395,"Ingrese la Letra a Adivinar=>");
+    fflush(stdin);
+    scanf("%c", Letra_Ingresada);
+    fflush(stdin);
+//     printf("%c", *Letra_Ingresada);  
+    fflush(stdin);    
+    
+}
+
+
+void Comprobar_y_Colocar_Letra (char Letra_Ingresada, char *Palabra, int Cantidad_de_Letras, int Largo_Cada_Linea, int Espacio_por_cada_Linea, int *Total_Encontradas, int PosX_Ahorcado, int PosY_Ahorcado) {
+    
+    int Cant_Letras_Encontradas=0, i;
+    
+    for(i=0;i<=Cantidad_de_Letras;i++) {
+    
+        if(Palabra[i]==Letra_Ingresada) {
+            Cant_Letras_Encontradas++;
+            Imprimir_Letra(Palabra, i, Letra_Ingresada, Largo_Cada_Linea, Espacio_por_cada_Linea, PosX_Ahorcado, PosY_Ahorcado); //COMPLETAR ESTE LLAMADO DE FUNCION
+        }
+        
+    }
+        *Total_Encontradas=(*Total_Encontradas)+Cant_Letras_Encontradas;
+        //printf("%i",Cant_Letras_Encontradas);
+        
+}
+
+
+void Imprimir_Letra (char *Palabra, int Posicion, char Letra_Ingresada, int Largo_Cada_Linea, int Espacio_por_cada_Linea, int PosX_Ahorcado, int PosY_Ahorcado) {
+    //ACÁ TENDRÍA QUE IMPRIMIR LA LETRA QUE SE ENCONTRO SEGUN LA POSICION EN EL STRING DE 'PALABRA'. LA POSICION EN EL MAPA SE PUEDE SABER CON EL VALOR DE 'LARGO_DE_LINEA' QUE SE DIVIDE A LA MITAD Y LOS ESPACIO_POR_cada_Linea PARA CALCULAR EL ESPACIO JUSTO DONDE LLEGA (PORQUE SINO NO VA A CONTAR LOS ESPACIO Y VA A TERMINAL MAL)
+    
+    char Letra[120];
+    sprintf(Letra,"%c",Letra_Ingresada); //COLOCA EN LA VARIABLE 'Letra' EL FORMATO CORRECTO PARA QUE OUTTEXTXY PUEDA IMPRIMIR CORRECTAMENTE 'Letra_Ingresada'
+    
+//     printf("%i", PosX_Ahorcado + (Largo_Cada_Linea/2) + ((Largo_Cada_Linea + Espacio_por_cada_Linea)*Posicion) );
+    
+    
+    outtextxy( (PosX_Ahorcado + Largo_Cada_Linea/2 + Largo_Cada_Linea*Posicion + Espacio_por_cada_Linea*Posicion) - 1 , PosY_Ahorcado-30 ,   Letra   );
+    
+    
+    
+    
+}
+
+
+
     
 int main () {
-    int gdriver=DETECT, gmode, i, Cantidad_de_Letras=0;
-    char Palabra[99];
+    int gdriver=DETECT, gmode, i, Cantidad_de_Letras=0,Largo_Cada_Linea,Espacio_por_cada_Linea, Total_Encontradas=0;
+    char Palabra[99], Letra_Ingresada='~';
     initgraph(&gdriver,&gmode,NULL);
     
     
     for(i=0;i<99;i++) {
-//         Palabra[i]="\t";
         Palabra[i]='~';
     }
     
-    Menu_Inicial();
+    //Menu_Inicial();
     
-    Pregunta(&Palabra,&Cantidad_de_Letras);
+     Pregunta(&Palabra,&Cantidad_de_Letras);
     
+    closegraph(); // CIERRO Y VUELVO A ABRIR PARA QUE NO SE CUELGUE LA LECTURA DEL TEXTO
+    gdriver=DETECT; gmode=7589738954937;
+    initgraph(&gdriver,&gmode,NULL);
+    
+    //while(Juego_Terminado!=1){
     Poste_Ahorcado(60,20,120);
-    Lineas_Palabras(100,350,getmaxx()-100,Cantidad_de_Letras);
+    Lineas_Palabras(100,350,getmaxx()-100,Cantidad_de_Letras, &Largo_Cada_Linea,&Espacio_por_cada_Linea);
+    Lee_Letra(&Letra_Ingresada);
     
     
-    
+    Comprobar_y_Colocar_Letra(Letra_Ingresada,&Palabra,Cantidad_de_Letras,Largo_Cada_Linea,Espacio_por_cada_Linea, &Total_Encontradas, 100/*POSICION 'x' DE LAS LINEAS DEL AHORCADO*/, 350/*POSICION 'y' DE LAS LINEAS DEL AHORCADO*/);
+
     delay(3000);
     closegraph();
+    
 }
